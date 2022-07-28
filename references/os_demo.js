2782b849479dda72b46ff704ed7ba3bc530a0d23
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-28 16:54:15 +0300

    os demo

---
+++ references/os_demo.js
@@ -0,0 +1,12 @@
+// OS Module 
+
+const os = require('os');
+
+// Get platform
+console.log(os.platform());
+
+// CPU Architecture
+console.log(os.arch());
+
+// CPU core info 
+console.log(os.cpus());
